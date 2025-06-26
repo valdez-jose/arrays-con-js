4e6c0ed150b8267334096833e78da2b5a85b5502
valdez-jose <jusepetony7@gmail.com>
2025-06-26 17:24:05 -0300

    Agregar ejercicio2.js y ejercicio1.js modificado todo a la carpeta for

--- basicos/for/ejercicio1.js
+++ basicos/for/ejercicio1.js
@@ -1,14 +1,7 @@
+const numeros = [];
 
-console.log("\nCrearemos un array vacío y llenarlo con números pares\n")
-// 
-console.log("Usaremos for para incrementar números pares de 2 en 2 hasta 10\n");
-const pares = [];
+numeros.push(10);
+numeros.push(20);
+numeros.push(30);
 
-for (let i = 1; i <= 10; i++) {
-  if (i % 2 === 0) {
-    pares.push(i);
-  }
-}
-
-console.log("Números pares:", pares); // [2, 4, 6, 8, 10]
-console.log("\n========Fin del ejercicio===========\n")
+console.log(numeros); // [10, 20, 30]
